873b60342b5fcd74d034f011e62fdc153ed55c3e
Patipat C. <patipat.che@mahidol.edu>
2024-09-22 11:57:21 +0700

    feat: add melisearch

--- my-store/medusa-config.js
+++ my-store/medusa-config.js
@@ -52,6 +52,35 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: `medusa-plugin-meilisearch`,
+    options: {
+      config: {
+        host: process.env.MEILISEARCH_HOST,
+        apiKey: process.env.MEILISEARCH_API_KEY,
+      },
+      settings: {
+        products: {
+          indexSettings: {
+            searchableAttributes: [
+              "title", 
+              "description",
+              "variant_sku",
+            ],
+            displayedAttributes: [
+              "id", 
+              "title", 
+              "description", 
+              "variant_sku", 
+              "thumbnail", 
+              "handle",
+            ],
+          },
+          primaryKey: "id",
+        },
+      },
+    },
+  }
 ];
 
 const modules = {
